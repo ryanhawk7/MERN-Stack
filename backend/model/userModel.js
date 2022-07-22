37cd019a928274cef02eb7f488cb1a590991a511
ryanhawk7 <93562350+ryanhawk7@users.noreply.github.com>
2022-07-21 18:03:21 -0600

    .

---
+++ backend/model/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'Please add a name']
+  },
+  email: {
+    type: String,
+    require: [true, 'Please add an email'],
+    uniqure: true
+  },
+  password: {
+    type: String,
+    require: [true, 'Please add a password']
+  },
+  name: {
+    type: String,
+    require: [true, 'Please add a name']
+  },
+},
+{
+  timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
